f8fbd010be5e17e4714a40c0a99ba3c00f5f599d
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-11 22:37:20 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,8 @@
+food=input("Name a food > ")
+plant=input("Name a type of plant > ")
+method=input("Name a method of cooking > ")
+burned=input("What word describes burned food? > ")
+DIY=input("Name a DIY item > ")
+print("\n")
+m=input(" ")
+print(method ,food ,"with ",burned ,plant ,"on a bed of ",DIY)
