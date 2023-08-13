f47632a146515bee058736084977da0b566e4a68
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-13 16:07:24 +0530

    Add files via upload

---
+++ day4.py
@@ -0,0 +1,9 @@
+print(
+    "Welcome to your adventure simulator. I am going to ask you a bunch of questions and then create an epic story with you as the star!"
+)
+n = input("What is your name?")
+w = input("What is your worst enemy’s name?")
+s = input("What is your superpower?")
+l = input("Where do you live?")
+f = input("What is your favorite food?")
+print("Hello", "\033[34m", n, "\033[0m", "! Your ability to", "\033[32m", s,"\033[0m", "will make sure you never have to look at","\033[31m", w,"\033[0m","again. Go eat","\033[33m", f,"\033[0m", "as you walk down the streets of", l, " and use", s," for good and not evil!")
