f96ead206e7aa1d6655b35cbdeb0a0a956113759
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-16 21:31:07 +0530

    Add files via upload

---
+++ day5.py
@@ -0,0 +1,16 @@
+print("Marel Movie Character Creator")
+sm=input("Do you like 'hanging around'?:")
+if(sm=="yes"|sm=="Yes"|sm=="YES"):
+  print("Then you're spider-man")
+else:
+  print("Then you're not spider-man")
+k=input("Do you have a'gravelly' voice?:")
+if(k=="yes"|k=="Yes"|k=="YES"):
+  print("Aww,then you're korg")
+else:
+  print("Aww,then you're not korg")
+m=input("Do you often feel 'marvelous'?:")
+if(m=="yes"|m=="Yes"|m=="YES"):
+  print("Aha! You're captain marvel!")
+else:
+  print("Aha! You're not captain marvel!")
