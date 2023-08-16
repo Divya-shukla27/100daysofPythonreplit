7f16f383f482ff1f695096ad983d11571c3b2970
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-16 22:07:28 +0530

    Add files via upload

---
+++ day7.py
@@ -0,0 +1,14 @@
+print("fake Indian patriot finder")
+w=int(input("how many wars between India and Pakistan"))
+if(w==4):
+  i=input("who won kargil war")
+  if(i=="India"):
+    k=input("who was the lead in kargil war")
+    if(k=="Vikram Batra"):
+      print("you are true Indian patriot")
+    else:
+      print("fake patriot")
+  else:
+      print("fake patriot")
+else:
+      print("fake patriot")
