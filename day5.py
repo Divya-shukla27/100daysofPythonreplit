adafed5928a0ffebed39bc9fe2a6c9b458afdd73
Divya-shukla27 <141561828+Divya-shukla27@users.noreply.github.com>
2023-08-16 21:41:38 +0530

    Update day5.py

--- day5.py
+++ day5.py
@@ -1,16 +1,16 @@
 print("Marel Movie Character Creator")
 sm=input("Do you like 'hanging around'?:")
-if(sm=="yes"|sm=="Yes"|sm=="YES"):
+if(sm=="yes" or sm=="Yes" or sm=="YES"):
   print("Then you're spider-man")
 else:
   print("Then you're not spider-man")
 k=input("Do you have a'gravelly' voice?:")
-if(k=="yes"|k=="Yes"|k=="YES"):
+if(k=="yes" or k=="Yes" or k=="YES"):
   print("Aww,then you're korg")
 else:
   print("Aww,then you're not korg")
 m=input("Do you often feel 'marvelous'?:")
-if(m=="yes"|m=="Yes"|m=="YES"):
+if(m=="yes" or m=="Yes" or m=="YES"):
   print("Aha! You're captain marvel!")
 else:
   print("Aha! You're not captain marvel!")
